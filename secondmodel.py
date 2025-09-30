3bfbae113177311712ab2dada6669970b0046a6c
Girishkumar yadagiri <yadagirigiri29@gmail.com>
2025-09-30 21:25:14 +0530

    Update Secondmodel.py

--- secondmodel.py
+++ secondmodel.py
@@ -1,1 +1,8 @@
-
+#this is SecondModel.py
+print("This is the second model")
+print(10+20)
+a = 15 
+b = 25
+print(a+b)
+print("Update in second model is done")
+print("This is the second model - update 2")
